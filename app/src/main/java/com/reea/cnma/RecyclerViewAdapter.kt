99f195ed889471a79cfde2601917811ecc1fd335
enricocirstea <enricocirstea@gmail.com>
2020-07-07 18:56:13 +0300

    RecyclerView Adapter Done

---
+++ app/src/main/java/com/reea/cnma/RecyclerViewAdapter.kt
@@ -0,0 +1,29 @@
+package com.reea.cnma
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recyclelist_item.view.*
+
+class RecyclerViewAdapter(private val exampleList: List<ExampleItem>) : RecyclerView.Adapter<RecyclerViewAdapter.ExampleViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recyclelist_item, parent, false)
+        return ExampleViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
+        val currentItem = exampleList[position]
+        holder.textView1.text = currentItem.text1
+        holder.textView2.text = currentItem.text2
+    }
+
+    override fun getItemCount() = exampleList.size
+
+    class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView1: TextView = itemView.testText1
+        val textView2: TextView = itemView.testText2
+    }
+}
