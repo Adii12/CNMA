8d38a7aab7e1d1ebeb660537c014cdfe80133194
Adrian Cimpean <cimpean_adrian12@yahoo.com>
2020-07-15 14:04:42 +0300

    Added search functionality

--- app/src/main/java/com/reea/cnma/viewModels/ViewModelFactory.kt
+++ app/src/main/java/com/reea/cnma/viewModels/ViewModelFactory.kt
@@ -5,13 +5,12 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class ViewModelFactory(private val movieTitle : String, var application: Application, var activity : Activity) : ViewModelProvider.NewInstanceFactory() {
+class ViewModelFactory(private val text : String, var application: Application, private var activity : Activity) : ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return when(modelClass) {
-            MovieDetailsScreenViewModel::class.java -> MovieDetailsScreenViewModel(application, movieTitle)
+            MovieDetailsScreenViewModel::class.java -> MovieDetailsScreenViewModel(application, text)
 
             FavoritesScreenViewModel::class.java -> FavoritesScreenViewModel(application, activity)
-
             else -> throw IllegalStateException("Unknown ViewModel Class")
         } as T
     }
